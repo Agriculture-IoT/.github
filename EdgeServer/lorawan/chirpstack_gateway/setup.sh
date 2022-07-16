b67f970b5ab5f6d5c4c6014fd43c7141cc3e1edd
chaudhariatul <103202802+chaudhariatul@users.noreply.github.com>
2022-07-15 17:07:08 -0700

    Delete EdgeServer/lorawan/chirpstack_gateway directory

--- EdgeServer/lorawan/chirpstack_gateway/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-
-sudo echo "deb https://artifacts.chirpstack.io/packages/3.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
-
-sudo DEBIAN_FRONTEND=noninteractive apt update -y 
-
-sudo DEBIAN_FRONTEND=noninteractive apt install chirpstack-gateway-bridge -y 
